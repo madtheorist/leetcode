c55f39b441d8138d6231b2aa9a9f2b7cc6ae6555
Jesse Xijia Wang <jessewang828@gmail.com>
2024-04-13 16:42:10 +0100

    house robber

--- solutions/dynamic_programming.py
+++ solutions/dynamic_programming.py
@@ -31,6 +31,34 @@ class DP:
             min_cost = c + min(cost_cache[0], cost_cache[1])
             cost_cache.appendleft(min_cost)
         return min(cost_cache[0], cost_cache[1])
+
+    @classmethod
+    def rob(cls, nums: List[int]) -> int:
+        """
+        https://leetcode.com/problems/house-robber/description/
+        Logic:
+        - go in order
+        - if you rob a given house, then you must skip the next one
+        Starting chains:
+        - R - 
+        - n - R - 
+        then skip either two houses or skip one.
+        """
+        # money_cache[i] is max money gained dfrom to robbing ith house and any houses before
+        if len(nums) == 1:
+            return nums[0]
+        if len(nums) == 2:
+            return max(nums)
+        if len(nums) == 3:
+            return max(nums[1], nums[0] + nums[2])
+        money_cache = [nums[0], nums[1], nums[0] + nums[2]]
+        for num in nums[3:]:
+            max_money = num + max(money_cache[-2], money_cache[-3])
+            money_cache.append(max_money)
+        return max(money_cache[-1], money_cache[-2])
+
+    
+
     
 
         
--- tests/test_dynamic_programming.py
+++ tests/test_dynamic_programming.py
@@ -1,19 +1,32 @@
 import pytest
 from solutions.dynamic_programming import DP
 
+
 def test_climb_stairs():
     assert DP.climbStairs(1) == 1
     assert DP.climbStairs(2) == 2
     for n in range(3, 46):
-        assert DP.climbStairs(n) == (DP.climbStairs(n-1) + DP.climbStairs(n-2))
+        assert DP.climbStairs(n) == (DP.climbStairs(n - 1) + DP.climbStairs(n - 2))
 
-@pytest.mark.parametrize("cost, output", [
-    ([20, 10], 10),
-    ([10, 20], 10),
-    ([10, 15, 20], 15),
-    ([1, 100, 1, 1, 1, 100, 1, 1, 100, 1], 6),
-    ([0, 0, 1, 0], 0),
-    ([0,0,1,1], 1)
-    ])
+
+@pytest.mark.parametrize(
+    "cost, output",
+    [
+        ([20, 10], 10),
+        ([10, 20], 10),
+        ([10, 15, 20], 15),
+        ([1, 100, 1, 1, 1, 100, 1, 1, 100, 1], 6),
+        ([0, 0, 1, 0], 0),
+        ([0, 0, 1, 1], 1),
+    ],
+)
 def test_min_cost_climbing_stairs(cost, output):
     assert DP.minCostClimbingStairs(cost) == output
+
+
+@pytest.mark.parametrize(
+    "nums, output",
+    [([1, 2, 3, 1], 4), ([2, 7, 9, 3, 1], 12), ([1, 100, 0, 0, 100], 200)],
+)
+def test_rob(nums, output):
+    assert DP.rob(nums) == output
